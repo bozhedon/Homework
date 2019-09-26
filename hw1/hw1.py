4ffb197dd797ea596a2996cd0ae1053f761c940a
bozhedon <bozhedon-yan@yandex.ru>
2019-09-27 00:44:52 +0900

    Update hw1.py
    Fix

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,15 +1,13 @@
 def get_digit_sum(n):
     summa = 0
-    m = int(n)
-    if m < 0:
-        m = - m
-    n = str(m)
-    for i in range(len(n)):
-        summa = summa + m % 10
-        m = m // 10
+    if n < 0:
+        n = - n
+    while n > 0:
+        summa = summa + n % 10
+        n = n // 10
     return summa
 
 
 if __name__ == '__main__':
-    number = input('Введите число: ')
+    number = int(input('Введите число: '))
     get_digit_sum(number)
