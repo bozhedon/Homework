d9ffd06f9aba6aba3a23542e99dc181dd35d8a08
bozhedon <bozhedon-yan@yandex.ru>
2019-10-04 23:18:02 +0900

    fix 5 task

--- hw2/hw5.py
+++ hw2/hw5.py
@@ -1,11 +1,13 @@
 def is_valid(braces_string):
-    lb, rb = 0, 0
+    n = 0
     for i in range(len(braces_string)):
         if braces_string[i] == '(':
-            lb += 1
+            n += 1
         if braces_string[i] == ')':
-            rb += 1
-    return lb == rb
+            n -= 1
+        if n < 0:
+            return False
+    return n == 0
 
 
 if __name__ == '__main__':
